c1da12185f9e8fcf82c0013de640a76d405f6370
Maxdev01 <61092309+Maxdev01@users.noreply.github.com>
2020-07-18 09:27:22 -0700

    mise en ligne

--- Elearning/settings.py
+++ Elearning/settings.py
@@ -75,14 +75,14 @@ WSGI_APPLICATION = 'Elearning.wsgi.application'
 
 #Database
    #https://docs.djangoproject.com/en/3.0/ref/settings/databases
-
+'''
 DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.sqlite3',
          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
      }
  }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -129,7 +129,7 @@ STATICFILES_DIRS =(
 
 import django_heroku
 import dj_database_url
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -142,6 +142,6 @@ DATABASES = {
 }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env) """
+DATABASES['default'].update(db_from_env) 
 
 django_heroku.settings(locals())
